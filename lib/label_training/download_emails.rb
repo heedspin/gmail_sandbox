ea206032192f415a0a67e4e3cb3bc2b6fa7f4074
Tim Harrison <heedspin@gmail.com>
2024-12-08 11:24:25 -0500

    Starting to build real gmail parsing.

--- lib/label_training/download_emails.rb
+++ lib/label_training/download_emails.rb
@@ -1,4 +1,4 @@
-# LabelTraining::DownloadEmails.new(User.first).run('1OWL24', limit: 50)
+# LabelTraining::DownloadEmails.new(User.first).run('1OWL24', limit: 100)
 class LabelTraining::DownloadEmails
   include Plutolib::LoggerUtils
   include WithGoogleApi
@@ -34,7 +34,7 @@ class LabelTraining::DownloadEmails
         #list_user_threads(user_id, include_spam_trash: nil, label_ids: nil, max_results: nil, page_token: nil, q: nil, fields: nil, quota_user: nil, user_ip: nil, options: nil) {|result, err| ... } ⇒ Google::Apis::GmailV1::ListThreadsResponse
         page_token = nil
         begin
-          result = @gmail.list_user_threads('me', label_ids: [label.id], max_results: 1, page_token: page_token)
+          result = @gmail.list_user_threads('me', label_ids: [label.id], max_results: 10, page_token: page_token)
           result.threads.each do |thread|
             buffer = self.download_thread(thread)
             self.save_thread(label, thread, buffer)
@@ -45,7 +45,7 @@ class LabelTraining::DownloadEmails
             end
           end
           page_token = result.next_page_token
-        end while (result.threads.size > 0)
+        end while page_token
       end
     end
     true
@@ -84,8 +84,34 @@ class LabelTraining::DownloadEmails
     end
     def subject
       header = @message_buffers.first.headers.find { |h| h.name == 'Subject' }
-      header.value
+      header.try(:value)
     end
+  end
+
+  # LabelTraining::DownloadEmails.new(User.first).debug_thread('1932b8cce68fa8b0')
+  def debug_thread(thread_id)
+    log "Debugging thread #{thread_id}"
+    result = nil
+    with_google_api(@user) do
+      result = @gmail.get_user_thread('me', thread_id)
+      debugger
+      true
+    end
+  end
+
+  # rails r "LabelTraining::DownloadEmails.new(User.first).marshal_thread('1932b8cce68fa8b0', 'spec/parse_gmail/nested_email.marshal')"
+  def marshal_thread(thread_id, sub_path)
+    log "Debugging thread #{thread_id}"
+    result = nil
+    with_google_api(@user) do
+      result = @gmail.get_user_thread('me', thread_id)
+    end
+    destination_file_path = Rails.root.join(sub_path)
+    File.open(destination_file_path, 'wb') do |file|
+      Marshal.dump(destination_file_path, file)
+    end
+    log "Wrote thread to #{destination_file_path}"
+    true
   end
 
   def download_thread(thread)
@@ -103,14 +129,15 @@ class LabelTraining::DownloadEmails
       message_buffer.label_ids = message.label_ids
       message_buffer.headers = message.payload.headers.select { |h| ['From', 'Date', 'Subject', 'To', 'Cc'].include?(h.name) }
       # "Headers=" + headers.map { |h| "#{h.name} #{h.value}" }.join("\n")
-      if message.payload.parts.nil?
-        log "No payload parts for message #{message.id}"
-      else
+      if message.payload.parts
         message.payload.parts.map do |part|
           if part.mime_type == 'text/html'
             message_buffer.add_body_part(part.body.data)
+            found_html = true
           end
         end
+      elsif message.payload.body
+        message_buffer.add_body_part(message.payload.body.data)
       end
       thread_buffer.add_message(message_buffer)
     end
@@ -127,22 +154,25 @@ class LabelTraining::DownloadEmails
   end
 
   def save_thread(label, thread, thread_buffer)
-    destination_folder = self.ensure_subfolder(thread_buffer.received_datecode)
+    destination_folder = self.ensure_subfolder(label.name.parameterize)
     destination_filename = "#{thread.id} #{thread_buffer.subject}".parameterize
     destination_file_path = File.join(destination_folder, destination_filename) + '.html'
     File.open(destination_file_path, 'w') do |file|
       file.puts '========================================================'
       thread_buffer.message_buffers.each do |message_buffer|
-        message_buffer.label_names(@labels_cache).each do |label|
-          file.puts "Label: #{utf8_encode label}"
-        end
+        # message_buffer.label_names(@labels_cache).each do |label_name|
+        #   file.puts "Label: #{utf8_encode label_name}"
+        # end
+        file.puts("<div class=\"header\">")
+        file.puts("<span>Gmail Thread ID</span><span>#{thread.id}</span>")
         message_buffer.headers.each do |header|
-          file.puts("#{header.name}: #{utf8_encode header.value}")
+          file.puts("<span>#{header.name}:</span> <span>#{utf8_encode header.value}</span>")
         end
+        file.puts("</div>")
         message_buffer.body_parts.each do |part|
-          log "Body part: #{part}"
+          # log "Body part: #{part}"
           file.puts(utf8_encode part)
-          file.puts '-----------------------------------------------------'
+          # file.puts '-----------------------------------------------------'
         end
       end
     end
